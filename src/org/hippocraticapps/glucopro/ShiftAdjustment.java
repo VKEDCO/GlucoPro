a3179122336d7e387e5723eee4253daf53c35707
Jesse_V <jvictors@jessevictors.com>
2013-11-12 14:53:10 -0700

    minor expansion

--- src/org/hippocraticapps/glucopro/ShiftAdjustment.java
+++ src/org/hippocraticapps/glucopro/ShiftAdjustment.java
@@ -1,6 +1,24 @@
 package org.hippocraticapps.glucopro;
 
-public class ShiftAdjustment
-{
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
 
+public class ShiftAdjustment extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.recommendation);
+	}
+
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 }
