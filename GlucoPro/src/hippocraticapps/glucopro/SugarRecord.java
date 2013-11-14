eaca9e6a3187d71ba4a4e40b2d39c57008bcb76f
aeonicentity <aeonicentity@gmail.com>
2013-11-14 09:28:02 -0700

    More Classes

--- GlucoPro/src/hippocraticapps/glucopro/SugarRecord.java
+++ GlucoPro/src/hippocraticapps/glucopro/SugarRecord.java
@@ -4,20 +4,22 @@ import java.util.Date;
 
 public class SugarRecord extends Record{
 	// static storage vars
-	public static int shiftID;
-	public static boolean pre;
-	public static float level;
-	public static long  time;
+	public int id;
+	public int shiftID;
+	public boolean pre;
+	public float level;
+	public long  time;
 	
 	public String parseTimeUnix(){ //will return a string of the date represented by the timestamp.
 		// We may want to change this later to a custom format. This will do for now.
-		// Saves you a ton of time on the front end Jesse.
+		// Saves you a ton of time on the front end, Jesse.
 		Date time = new Date(this.time);
 		return time.toString();
 	}
 	
-	public SugarRecord( int shiftID, boolean pre, float level, long time ){
+	public SugarRecord( int id, int shiftID, boolean pre, float level, long time ){
 		// set values
+		this.id = id;
 		this.shiftID = shiftID;
 		this.pre = pre;
 		this.level = level;
--- GlucoPro/src/hippocraticapps/glucopro/SugarTable.java
+++ GlucoPro/src/hippocraticapps/glucopro/SugarTable.java
@@ -7,5 +7,10 @@ public class SugarTable implements DatabaseTable{
 		// TODO Auto-generated method stub
 		
 	}
-
+	static SugarRecord[] peekRange( GlucoDBAdapter adptr,int maxSize ){
+		SugarRecord[] records = new SugarRecord[maxSize];
+		//make an adapter query call to get the nth entry from the top, starting newest first
+		
+		return records; // return array of records
+	}
 }
