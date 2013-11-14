f797a2bbd50ffc3ce6657419978e7361d764251b
aeonicentity <aeonicentity@gmail.com>
2013-11-14 08:51:54 -0700

    Added the DatabaseTable class
    Also SugarTable

---
+++ GlucoPro/src/hippocraticapps/glucopro/SugarTable.java
@@ -0,0 +1,11 @@
+package hippocraticapps.glucopro;
+
+public class SugarTable implements DatabaseTable{
+
+	@Override
+	public void insert(GlucoDBAdapter adptr, Record record) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
